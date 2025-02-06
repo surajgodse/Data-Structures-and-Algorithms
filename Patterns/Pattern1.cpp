bcceab8cd3566a1854062937b12714de35372cdc
Suraj Godse <godsesuraj2140@gmail.com>
2025-02-05 19:22:12 -0600

    Initial commit with DSA files

---
+++ Patterns/Pattern1.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int n = 4;
+
+for(int i = 0; i <n; i++){
+    char ch = 'A';
+    for(int j = 0; j<n; j++){
+        cout << ch;
+         ch = ch + 1;
+    }
+       cout<< endl;
+}
+}
