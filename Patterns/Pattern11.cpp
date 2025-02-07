34e21c607cc27faa0a1573d7a930cb8060d8412b
Suraj Godse <godsesuraj2140@gmail.com>
2025-02-06 19:00:51 -0600

    pattern commit

---
+++ Patterns/Pattern11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n = 4;
+
+    for(int i = 0; i <n; i++){
+        //spaces
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+
+        //number
+        for(int j = 0; j<n-i; j++){
+            cout << (i + 1);
+        }
+        cout << endl;
+
+    }
+}
---
+++ Patterns/Pattern9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int n = 4;
+char ch = 'A';
+for(int i = 0; i < n; i++){
+    for(int j = 1; j <= i+1; j++){ //{int j = i+1; j>0; j--} - BACKWARD
+        cout << ch << " ";
+        ch  = ch + 1;
+        }
+    cout<< endl;
+    }
+}
